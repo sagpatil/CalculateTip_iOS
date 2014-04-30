c847f5bc60fcbaa0d7dfc99f06cb25a04c76d90c
Patil <sagpatil@LM-SJN-00874485.(none)>
2014-04-30 15:20:49 -0700

    Save retrieve values from Settings, give default values of Tips, Clear Text fields onClickEvent
    V0.1

--- CalculateTip/CalculateTip/SettingsViewController.m
+++ CalculateTip/CalculateTip/SettingsViewController.m
@@ -9,11 +9,13 @@
 #import "SettingsViewController.h"
 
 @interface SettingsViewController ()
-- (IBAction)onTap:(id)sender;
+
+- (IBAction)onTapSettings:(id)sender;
 - (void)saveValues;
 @property (weak, nonatomic) IBOutlet UITextField *tip1TextField;
 @property (weak, nonatomic) IBOutlet UITextField *tip2TextField;
-@property (weak, nonatomic) IBOutlet UITextField *tip3textField;
+@property (weak, nonatomic) IBOutlet UITextField *tip3TextField;
+- (IBAction)onTipTextFieldTouched:(id)sender;
 
 @end
 
@@ -28,10 +30,30 @@
     return self;
 }
 
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    //  retrieve saved values
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    float tip1 =[defaults floatForKey:@"tip1"];
+    float tip2 =[defaults floatForKey:@"tip2"];
+    float tip3 =[defaults floatForKey:@"tip3"];
+    if (tip1 == 0.0 && tip2 ==0.0 && tip3 ==0.0) {
+        tip1 =5.0;tip2=10.0;tip3=20.0;
+    }
+
+    self.tip1TextField.text = [NSString stringWithFormat:@"%.2f", tip1];
+    self.tip2TextField.text = [NSString stringWithFormat:@"%.2f", tip2];
+    self.tip3TextField.text = [NSString stringWithFormat:@"%.2f", tip3];
+    
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+      NSLog(@"Back to Tips button clicked");
+     [self saveValues];
 }
 
 - (void)didReceiveMemoryWarning
@@ -40,7 +62,9 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)onTap:(id)sender {
+
+- (IBAction)onTapSettings:(id)sender {
+ 
     [self.view endEditing:YES];
     [self saveValues];
     
@@ -50,12 +74,16 @@
 - (void)saveValues{
     float tip1 = [self.tip1TextField.text floatValue];
     float tip2 = [self.tip2TextField.text floatValue];
-    float tip3 = [self.tip3textField.text floatValue];
+    float tip3 = [self.tip3TextField.text floatValue];
     
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     [defaults setFloat:tip1 forKey:@"tip1"];
     [defaults setFloat:tip2 forKey:@"tip2"];
     [defaults setFloat:tip3 forKey:@"tip3"];
     [defaults synchronize];
+}
+- (IBAction)onTipTextFieldTouched:(id)sender {
+    UITextField *tf = (UITextField *)sender;
+    tf.text = @"";
 }
 @end
--- CalculateTip/TipViewController.m
+++ CalculateTip/TipViewController.m
@@ -15,6 +15,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *amountLabel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+- (IBAction)onSegmentControlValueChange:(id)sender;
 - (void) updateValues;
 -(void)onSettingsButton;
 
@@ -34,11 +35,18 @@
 - (void) viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+    
     NSTimer *timer1=[NSTimer timerWithTimeInterval:10.1 target:self selector:@selector(changeBackgroundColor) userInfo:nil repeats:YES];
     
     NSRunLoop *mainLoop = [NSRunLoop mainRunLoop];
     [mainLoop addTimer:timer1 forMode:NSDefaultRunLoopMode];
     
+    
+    
+    
+
+    
+
 }
 CGFloat redValue;
 CGFloat greenValue=255;
@@ -67,10 +75,10 @@ CGFloat blueValue;
         blueValue = 255;
     }
     
-    NSLog(@"Red : %.f  Green: %f  Blue:  %f",redValue,greenValue,blueValue);
+    //NSLog(@"Red : %.f  Green: %f  Blue:  %f",redValue,greenValue,blueValue);
     
     
-    self.view.backgroundColor = [UIColor colorWithRed:redValue green:greenValue blue:blueValue alpha:1.0];
+    //self.view.backgroundColor = [UIColor colorWithRed:redValue green:greenValue blue:blueValue alpha:1.0];
 }
 
 - (void)viewDidLoad
@@ -78,7 +86,38 @@ CGFloat blueValue;
     [super viewDidLoad];
     [self updateValues];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Settings" style:UIBarButtonItemStylePlain target:self action:@selector(onSettingsButton)];
+  
+}
 
+- (void)viewWillAppear:(BOOL)animated {
+       NSLog(@"viewMain will appear");
+    //  retrieve saved values
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    float tip1 =[defaults floatForKey:@"tip1"];
+    float tip2 =[defaults floatForKey:@"tip2"];
+    float tip3 =[defaults floatForKey:@"tip3"];
+    if (tip1 == 0.0 && tip2 ==0.0 && tip3 ==0.0) {
+        tip1 =5.0;tip2=10.0;tip3=20.0;
+    }
+    NSString *strTip1= [NSString stringWithFormat:@"%.2f", tip1];
+    NSString *strTip2= [NSString stringWithFormat:@"%.2f", tip2];
+    NSString *strTip3= [NSString stringWithFormat:@"%.2f", tip3];
+    
+    NSLog(@"Tips %f  %f %f",tip1,tip2,tip3);
+    
+    //set values to the SegmentedControl
+    [self.tipControl setTitle:strTip1 forSegmentAtIndex:0];
+    [self.tipControl setTitle:strTip2 forSegmentAtIndex:1];
+    [self.tipControl setTitle:strTip3 forSegmentAtIndex:2];
+
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+  //  NSLog(@"view will disappear");
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+  //  NSLog(@"view did disappear");
 }
 
 - (void)didReceiveMemoryWarning
@@ -88,27 +127,49 @@ CGFloat blueValue;
 }
 
 - (IBAction)ontap:(id)sender {
+       NSLog(@"HEre");
     [self.view endEditing:YES];
     [self updateValues];
 }
 
 - (void)updateValues
 {   float billAmount = [self.billTextField.text floatValue];
-    NSArray *tipValues = @[@(0.1),@(0.15),@(0.2)];
-    float tipAmount = billAmount*[tipValues[self.tipControl.selectedSegmentIndex] floatValue];
+    
+  
+
+    NSString *strTip1 =  [self.tipControl titleForSegmentAtIndex:0];
+      NSString *strTip2 =  [self.tipControl titleForSegmentAtIndex:1];
+      NSString *strTip3 =  [self.tipControl titleForSegmentAtIndex:2];
+//    NSLog(@"Segtitle :%@",str);
+    float tip1, tip2, tip3;
+    tip1 = [strTip1 floatValue];
+    tip2 = [strTip2 floatValue];
+    tip3 = [strTip3 floatValue];
+    
+    
+    NSArray *tipValues = @[@(tip1),@(tip2),@(tip3)];
+    
+    float tipAmount = billAmount*[tipValues[self.tipControl.selectedSegmentIndex] floatValue]/100;
+    NSLog(@"Tip :%f",tipAmount);
     float totalAmount = tipAmount + billAmount;
     
     self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tipAmount];
     self.amountLabel.text = [NSString stringWithFormat:@"$%.2f", totalAmount];
     
-    // Test retrieve saved values
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    float tip1 =[defaults floatForKey:@"tip1"];
-    NSLog(@" tip %f",tip1);
+    
+    
+    
 }
 
 - (void)onSettingsButton{
     [self.navigationController pushViewController:[[SettingsViewController alloc]init] animated:YES];
 }
 
+- (IBAction)onSegmentControlValueChange:(id)sender {
+    UISegmentedControl *segmentedControl = (UISegmentedControl *)sender;
+    NSString *val = [segmentedControl titleForSegmentAtIndex: [segmentedControl selectedSegmentIndex]];
+    NSLog(@"Current Tip value %@ ",val);
+    [self updateValues];
+
+}
 @end
